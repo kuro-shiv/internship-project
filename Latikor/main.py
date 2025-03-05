816b6981a56ddf94e7c8e884272f0092b78230a8
Shivam Kumar Dubey <167622583+kuro-shiv@users.noreply.github.com>
2025-03-05 18:56:50 +0530

    Update main.py

--- Latikor/main.py
+++ Latikor/main.py
@@ -10,8 +10,3 @@ driver.get("https://www.saucedemo.com/")
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
-
-
-print("✅ Login Successful!" if "inventory.html" in driver.current_url else "❌ Login Failed!")
-
-input("Press Enter to exit...")  # Keeps browser open
